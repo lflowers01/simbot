15b2a029f42ba91870099d3e9ab1273de3cff7bb
Lucas Flowers <lucasdflowers@gmail.com>
2025-08-28 18:00:02 -0700

    elevator mech 2d working

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,23 +18,30 @@ public class Constants {
         public static double KG = 0.3;
         public static double KS = 0.4;
         public static double KV = 0.001;
+        public static double KF = 0.1;
         public static double KA = 0.0;
-        public static double KP = 0.8;
+        public static double KP = 3;
         public static double KI = 0;
         public static double KD = 0;
 
-        public static double motionVelocity = 0;
-        public static double motionAcceleration = 0;
+        public static double motionVelocity = 5;
+        public static double motionAcceleration = 10;
         public static double EXPO_KV = 0.04;
         public static double EXPO_KA = 0.005;
 
         public static double IDLE = 0;
-        public static double L1 = 3;
-        public static double L2 = 5;
-        public static double L3 = 7;
-        public static double L4 = 10;
+        public static double L1 = 0.5;
+        public static double L2 = 1;
+        public static double L3 = 1.5;
+        public static double L4 = 2.5;
 
+        public static double elevatorGearing = 10;
+        public static double carriageMass = 1.76901;
+        public static double elevatorDrumRadius = 0.1;
+        public static double minElevatorHeightMeters = 0.5;
+        public static double maxElevatorHeightMeters = 3;
 
+        public static double ROTATIONS_PER_METER = 5;
     }
 
 }
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -7,12 +7,22 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicExpoDutyCycle;
 import com.ctre.phoenix6.controls.MotionMagicExpoVoltage;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.hardware.TalonFXS;
+import com.ctre.phoenix6.sim.TalonFXSimState;
+import com.google.flatbuffers.Constants;
 
-
-
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
@@ -25,71 +35,121 @@ public class Elevator extends SubsystemBase{
     TalonFX leadMotor, followMotor;
     
     TalonFXConfiguration leadConfig, followConfig;
-    final MotionMagicExpoVoltage elevatorControl;
+    final MotionMagicVoltage elevatorControl;
 
     // Simulation variables
-    Mechanism2d elevatorMech;
+    Mechanism2d elevatorBase;
     MechanismRoot2d elevatorRoot;
-    MechanismLigament2d elevator;
+    MechanismLigament2d elevatorDraw;
     double elevatorMinimumLength;
 
     double elevatorTarget;
 
+    TalonFXSimState leadMotorSimState;
+    TalonFXSimState followMotorSimState;
+    DCMotorSim leadMotorSim;
+    DCMotorSim followMotorSim;
+
+    ElevatorSim elevatorSim;
 
     public Elevator(double elevatorMinimumLength) {
 
         leadMotor = new TalonFX(constElevator.LEAD_MOTOR_ID);
         followMotor = new TalonFX(constElevator.FOLLOW_MOTOR_ID);
-        leadConfig = new TalonFXConfiguration();
+    
+        followMotorSimState = followMotor.getSimState();
+        leadMotorSimState = leadMotor.getSimState();
 
+        leadConfig = new TalonFXConfiguration();
+    
         leadConfig.Slot0.kG = constElevator.KG;
         leadConfig.Slot0.kS = constElevator.KS;
         leadConfig.Slot0.kV = constElevator.KV;
+        // leadConfig.Slot0.KF = constElevator.KF;
         leadConfig.Slot0.kA = constElevator.KA;
         leadConfig.Slot0.kP = constElevator.KP;
         leadConfig.Slot0.kI = constElevator.KI;
         leadConfig.Slot0.kD = constElevator.KD;
 
-        MotionMagicConfigs elevatorMotion = leadConfig.MotionMagic;
+        MotionMagicConfigs elevatorMotion = leadConfig.MotionMagic;        
 
         elevatorMotion.MotionMagicCruiseVelocity = constElevator.motionVelocity;
         elevatorMotion.MotionMagicAcceleration = constElevator.motionAcceleration;
-        elevatorMotion.MotionMagicExpo_kA = constElevator.EXPO_KA;
-        elevatorMotion.MotionMagicExpo_kV = constElevator.EXPO_KV;
+        
+        //elevatorMotion.MotionMagicExpo_kA = constElevator.EXPO_KA;
+        //elevatorMotion.MotionMagicExpo_kV = constElevator.EXPO_KV;
 
         leadMotor.getConfigurator().apply(leadConfig);
 
         followMotor.setControl(new Follower(leadMotor.getDeviceID(), true));
-        elevatorControl = new MotionMagicExpoVoltage(0);
+        elevatorControl = new MotionMagicVoltage(0);
         
         // Simulation setup, Mechanism2d
 
-        elevatorMech = new Mechanism2d(3, 3);
-        elevatorRoot = elevatorMech.getRoot("elevatorRoot", 2, 0);
-        elevator = elevatorRoot.append(new MechanismLigament2d("elevator", 2, 90));
+        elevatorBase = new Mechanism2d(3, 3);
+        elevatorRoot = elevatorBase.getRoot("elevatorRoot", 2, 0);
+        elevatorDraw = elevatorRoot.append(new MechanismLigament2d("elevator", 2, 90));
         this.elevatorMinimumLength = elevatorMinimumLength;
-        SmartDashboard.putData("Elevator", elevatorMech);
+        SmartDashboard.putData("Elevator", elevatorBase);
+        elevatorSim = new ElevatorSim(
+            DCMotor.getKrakenX60(2),
+            constElevator.elevatorGearing,
+            constElevator.carriageMass,
+            constElevator.elevatorDrumRadius,
+            constElevator.minElevatorHeightMeters,
+            constElevator.maxElevatorHeightMeters,
+            true,
+            0.5
+        );
 
-        this.setElevatorHeight(0);
+        //this.setElevatorHeight(0);
+
+
         
     }
 
 
     public Distance getElevatorHeight() {
-        return Units.Inches.of(leadMotor.getPosition().getValueAsDouble());
+        if (RobotBase.isReal() == false) {
+            return Units.Meters.of(elevatorSim.getPositionMeters());
+
+        } else{
+            return Units.Meters.of(leadMotor.getPosition().getValueAsDouble()/constElevator.ROTATIONS_PER_METER);
+        }
     }
 
-    public void setElevatorHeight(double height) {
-        leadMotor.setControl(elevatorControl.withPosition(height));
+    public void setElevatorHeight(double height) { // IN METERS
+        elevatorSim.setState(height, 0);
+        leadMotor.setControl(elevatorControl.withPosition(height * constElevator.ROTATIONS_PER_METER));
         elevatorTarget = height;
     }
 
+
+
     @Override
     public void periodic() {
-        elevator.setLength(elevatorMinimumLength + getElevatorHeight().in(Meters));
+        elevatorDraw.setLength(getElevatorHeight().in(Meters));
         SmartDashboard.putNumber("Elevator Height", getElevatorHeight().in(Meters));
         SmartDashboard.putNumber("Elevator Target", elevatorTarget);
+    
         
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        // Simple physics simulation
+        leadMotorSimState.setSupplyVoltage(RobotController.getBatteryVoltage());
+        followMotorSimState.setSupplyVoltage(RobotController.getBatteryVoltage());
+
+        elevatorSim.setInputVoltage(leadMotorSimState.getMotorVoltage());
+        elevatorSim.update(0.02);
+
+
+        leadMotorSimState.setRawRotorPosition(elevatorSim.getPositionMeters() * constElevator.ROTATIONS_PER_METER);
+        followMotorSimState.setRawRotorPosition(elevatorSim.getPositionMeters() * constElevator.ROTATIONS_PER_METER);
+
+        leadMotorSimState.setRotorVelocity(elevatorSim.getVelocityMetersPerSecond() * constElevator.ROTATIONS_PER_METER);
+        followMotorSimState.setRotorVelocity(elevatorSim.getVelocityMetersPerSecond() * constElevator.ROTATIONS_PER_METER);
         
     }
 }
